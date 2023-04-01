93320bd5d5b59774346e30049644c4f44b05b51f
Rayo1234 <rayomiracle@gmail.com>
2023-04-01 12:32:21 -0700

    alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,12 +8,11 @@
 int main(void)
 {
 	int i;
-
-	for (i = 97; i <= 122; i++)
+		for (i = 97; i <= 122; i++)
 		{
-			if(i != 113 && i != 101)
-			putchar(i);
+			if (i != 113 && i != 101)
+				putchar (i);
 		}
-	putchar('\n');
-	return(0);
+	putchar ('\n');
+	return (0);
 }
