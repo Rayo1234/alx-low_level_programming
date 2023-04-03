e2b0791ba1d8302d68c2fe53ca3fb3969b9afbcd
Rayo1234 <rayomiracle@gmail.com>
2023-04-03 09:09:13 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _mencpy - function copies n bytes from memory area src to memory area dest
+ * @*dest: is a pointer
+ * @src: is a memory area
+ * @n: is an integer
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *p = dest;
+	while(n--)
+	{
+		*p++ = *src++;
+	}
+	return (dest);
+
+}
