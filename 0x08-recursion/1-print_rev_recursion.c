dd0c7183774722880b0188cce339f97b7ecc6107
Rayo1234 <rayomiracle@gmail.com>
2023-04-05 08:16:49 -0700

    rev recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,11 +7,12 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-		{
-			putchar('\n');
-			return;
-		}
-	_print_rev_recursion(s + 1);
-	putchar(*s);
+	{
+		if (*s == '\0')
+		putchar('\n');
+		return;
+		--s;
+		_print_rev_recursion(s + 1);
+		putchar(*s);
+	}
 }
