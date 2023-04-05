fda404a496a4e832ac0c580638ccb310c82c31ab
Rayo1234 <rayomiracle@gmail.com>
2023-04-05 07:00:58 -0700

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion - used to get fibbonnocia
+ * s: is a character
+ * void: is the library
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{	putchar('\n');
+		return;
+	}
+		putchar(*s);
+		_puts_recursion(s + 1);
+
+	{
+		char str[] = "Puts with recursion";
+		_puts_recursion(str);
+	}
+	return;
+}
