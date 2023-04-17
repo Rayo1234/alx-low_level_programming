40ed86a95e061a9f42a276a8759837f5c45ca404
Rayo1234 <rayomiracle@gmail.com>
2023-04-17 08:45:43 -0700

    program

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define STR(s) #s
+#define FILE_LINE(file, line) STR(file) ":" STR(line)
+
+
+/**
+ * main - is a function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	const char *file_line = FILE_LINE(__FILE__, __LINE__);
+	const char *p = file_line;
+
+	while (*p)
+		putchar(*p++);
+
+	return(EXIT_SUCCESS);
+
+}
