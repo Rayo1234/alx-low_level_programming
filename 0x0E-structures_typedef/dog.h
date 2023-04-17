103a60f6e40a4bf319d22216981a4c04b6801738
Rayo1234 <rayomiracle@gmail.com>
2023-04-17 10:12:43 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - it is defines a new type
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ * Return: 0
+ */
 struct dog
 {
 	char *name;
