ad2f99136b415915c3b26a09cf97c8720b432f3e
Rayo1234 <rayomiracle@gmail.com>
2023-04-17 11:12:38 -0700

    printdog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,13 +8,15 @@
  * @owner: third member
  * Return: 0
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
 
+void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
 
