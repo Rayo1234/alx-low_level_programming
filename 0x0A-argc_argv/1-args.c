83de89488e9330b36334855dd3f5a9f111d46798
Rayo1234 <rayomiracle@gmail.com>
2023-04-24 12:35:44 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "main.h"
 
-/** main - print the bumber of arguments
+/**
+ * main - print the bumber of arguments
  * @argc: number of arguments
  * @argv: array of arguments
- *
  * Return: always 0
  */
 int main(int argc, char *argv[])
