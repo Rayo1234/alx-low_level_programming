64602dbc06d4dab682e1d3ac119798788a5a1011
Rayo1234 <rayomiracle@gmail.com>
2023-04-27 07:21:50 -0700

    length of list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,7 +5,7 @@
  * list_len - returns the number of element
  * @h: pointer to list_t list
  *
- * Return: number of elements in 
+ * Return: number of elements in
  */
 size_t list_len(const list_t *h)
 {
