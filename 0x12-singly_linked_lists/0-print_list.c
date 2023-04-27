d95b8a2a8d1b40233a9b3b495dd03356970ca03b
Rayo1234 <rayomiracle@gmail.com>
2023-04-27 07:20:03 -0700

    length of lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,12 +12,4 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (!h->str)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u]%s\n", h->len, h->str);
-		h = h->next;
-		s++;
-	}
-	return (s);
-}
+		if (!h->str
